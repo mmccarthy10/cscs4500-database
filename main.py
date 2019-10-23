b1c25310eb190f0e27f91f30abc51f594b71af46
BuildTools <unconfigured@null.spigotmc.org>
2019-10-23 06:08:26 -0500

    Removed deprecated code from main

--- main.py
+++ main.py
@@ -23,40 +23,14 @@ def Authenticate():
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         if "delete" in details:
             cursor.execute('DELETE FROM donation WHERE donationId="' + details['delete'] + '";')
-            mysql.connection.commit()
         if "name" in details:
             cursor.execute('INSERT INTO donation (donationName, quantity, donator, date) VALUES("' + details['name'] + '", "' + details['qty'] + '", "' + details['donator'] + '", "2019-10-22");')
-            mysql.connection.commit()
-    donationName = 'T-Shirt'
-    #cursor = mysql.connect().cursor()
-    #cursor.execute("SELECT * from donation where donationName='" + donationName + "'")
-    #data = cursor.fetchone()
+
+        mysql.connection.commit()
     cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
     cursor.execute('SELECT * FROM donation;')
     data = cursor.fetchall()
-    print("Routed here")
-    #entry1 = cursor.fetchone()
 
-    #cursor.execute('SELECT * FROM donation WHERE donationId=2')
-    #entry2 = cursor.fetchone()
-    #cursor.execute('SELECT * FROM donation WHERE donationId=3')
-    #entry3 = cursor.fetchone()
-    #if entry3 is None:
-    #    entry2['donationName'] = 'NULLED!'
-    
-    table = [
-        {
-           'name': 'T-Shirt',
-           'quantity': '4',
-           'donator': 'Sarah Hawkins',
-           'date': '2019-10-14',
-           'id': '1'
-        }
-    ]
-        #if data is None:
-     #return "Definitely not a thing."
-    #else:
-     #return "That's a thing!"
     return render_template('donations.html', data=data)
 
 if __name__ == "__main__":

6818b97888ce443a8c96b04673ed7702a7d13ea1
Carlos Galvan <galvancarlosjr@gmail.com>
2019-10-28 12:41:31 -0500

    variable update
    :)

--- main.py
+++ main.py
@@ -21,7 +21,8 @@ def login():
 #Carlos page
 @app.route("/carlos")
 def carlos():
- 	return render_template('carlos.html')
+	example = "Hello"
+ 	return render_template('carlos.html', example)
 
 # Main donation landing page, reads and writes data to donation table
 @app.route("/Donation", methods=['GET', 'POST'])
